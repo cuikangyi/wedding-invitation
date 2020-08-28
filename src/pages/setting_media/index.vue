b74e401c283c161defa56dc0c499ab20a1db62d9
cuikangyi <392083349@qq.com>
2020-08-28 17:17:27 +0800

    媒体设置

---
+++ src/pages/setting_media/index.vue
@@ -0,0 +1,121 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <button class="btn" @tap="chooseBackground">选择背景图</button>
+      <image :src="background" mode="heightFix"></image>
+    </div>
+    <div class="row">
+      <button class="btn" @tap="chooseMusic">选择音乐</button>
+      <text user-select>{{ musicUrl }}</text>
+    </div>
+    <div class="row">
+      <button class="btn" @tap="chooseVideo">是否显示视频</button>
+      <text user-select>{{ videoUrl }}</text>
+    </div>
+  </div>
+</template>
+
+<script>
+import upload from '../../utils/upload'
+
+export default {
+  name: 'SettingMedia',
+  data () {
+    return {
+      _id: '',
+      background: '',
+      musicUrl: '',
+      videoUrl: ''
+    }
+  },
+  onLoad () {
+    const db = wx.cloud.database()
+    const media = db.collection('media')
+    media.get().then(res => {
+      const data = res.data[0]
+      this._id = data._id
+      this.background = data.background
+      this.musicUrl = data.musicUrl
+      this.videoUrl = data.videoUrl
+    })
+  },
+  methods: {
+    saveData (data) {
+      wx.showLoading({
+        title: `数据保存中...`,
+        mask: true
+      })
+      const db = wx.cloud.database()
+      db.collection('media').doc(this._id).update({
+        data
+      }).then(res => {
+        wx.showToast({
+          title: '修改成功',
+          icon: 'none'
+        })
+        console.log(data, res)
+        Object.keys(data).map(k => {
+          this[k] = data[k]
+        })
+      })
+    },
+    chooseBackground () {
+      const that = this
+      wx.chooseImage({
+        count: 1,
+        sizeType: ['original', 'compressed'],
+        sourceType: ['album'],
+        success (res) {
+          const tempFilePath = res.tempFilePaths[0]
+          upload.uploadBackground(tempFilePath).then(res => {
+            that.saveData({
+              background: res
+            })
+          })
+        }
+      })
+    },
+    chooseMusic () {
+      wx.chooseMessageFile({
+        count: 1,
+        success: ({ tempFiles }) => {
+          upload.uploadMusic(tempFiles[0].name, tempFiles[0].path).then(res => {
+            this.saveData({
+              musicUrl: res
+            })
+          })
+        }
+      })
+    },
+
+    chooseVideo () {
+      const videoUrl = this.videoUrl ? '' : 'cloud://midou-1i5z7.6d69-midou-1i5z7-1302919217/common/video.m4v'
+      this.saveData({
+        videoUrl
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+.container{
+  min-width: 100vw;
+  min-height: 100vh;
+  box-sizing: border-box;
+  position: relative;
+}
+.row{
+  padding: 10rpx;
+}
+.btn{
+  width: 500rpx;
+  line-height: 80rpx;
+  border-radius: 80rpx;
+  text-align: center;
+  margin: 20rpx auto;
+}
+button::after{
+  border: none;
+}
+</style>
---
+++ src/utils/upload.js
@@ -0,0 +1,196 @@
+
+// 上传单个文件
+const uploadFile = (cloudPath, filePath) => {
+  return new Promise((resolve, reject) => {
+    wx.cloud.uploadFile({
+      cloudPath,
+      filePath,
+      success: function (res) {
+        resolve(res.fileID)
+      },
+      fail: function (err) {
+        reject(err)
+      }
+    })
+  })
+}
+// 获取链接地址
+const getUrl = ids => {
+  wx.showLoading({
+    title: `正在获取链接地址`,
+    mask: true
+  })
+  return new Promise((resolve, reject) => {
+    wx.cloud.getTempFileURL({
+      fileList: ids,
+      success: function ({ fileList }) {
+        resolve(
+          fileList.map(item => {
+            return {
+              id: item.fileID,
+              url: item.tempFileURL
+            }
+          })
+        )
+      },
+      fail: function (err) {
+        reject(err)
+      }
+    })
+  })
+}
+const getFileName = name => {
+  // return name
+  name = name.replace(/^\s+|\s+$/, '').replace(/\s+/g, '_')
+  const arr = name.split('.').reverse()
+  return `${arr[1].replace(/[/:]/g, '')}.${arr[0]}`
+}
+
+const uploadImg = filePaths => {
+  const ids = []
+  // 上传图片
+  let resolve = Promise.resolve()
+  for (let i = 0; i < filePaths.length; i++) {
+    resolve = resolve.then(() => {
+      const cloudPath = `photo/${getFileName(filePaths[i])}`
+      const path = filePaths[i]
+      if (i === 0) {
+        wx.showLoading({
+          title: `正在上传第1张图片`,
+          mask: true
+        })
+      }
+      return uploadFile(cloudPath, path)
+        .then(id => {
+          wx.showLoading({
+            title: `正在上传第${i + 2}张图片`,
+            mask: true
+          })
+          ids.push(id)
+        })
+        .catch(_ => {
+          wx.showLoading({
+            title: `正在上传第${i + 2}张图片`,
+            mask: true
+          })
+        })
+    })
+  }
+
+  // 获取真实链接地址
+  resolve = resolve.then(() => {
+    return getUrl(ids)
+  })
+  // 存储数据库
+  let page = getCurrentPages()
+  page = page[page.length - 1]
+  const { $photos } = page.data
+  resolve = resolve.then(res => {
+    // eslint-disable-next-line prefer-promise-reject-errors
+    if (!res.length) return Promise.reject('上传失败')
+    $photos.push(...res)
+    return $photos
+  })
+
+  // 返回数据
+  resolve = resolve
+    .then(() => {
+      wx.hideLoading()
+      return $photos
+    })
+    .catch(err => {
+      wx.hideLoading()
+      wx.showToast({
+        title: err,
+        icon: 'none'
+      })
+      return Promise.reject(err)
+    })
+  return resolve
+}
+
+const delImg = id => {
+  // 修改数据库
+  /* let page = getCurrentPages()
+  page = page[page.length - 1]
+  let { $photos } = page.data
+  $photos = $photos.filter(item => item.id !== id) */
+  del([id]).catch(null)
+}
+const del = ids => {
+  return new Promise((resolve, reject) => {
+    wx.cloud.deleteFile({
+      fileList: ids,
+      success: function ({ fileList }) {
+        resolve(fileList.map(item => item.fileID))
+      },
+      fail: function (err) {
+        reject(err)
+      }
+    })
+  })
+}
+
+const uploadMusic = (name, filePath) => {
+  const fileName = getFileName(name)
+  const cloudPath = `music/${fileName}`
+  wx.showLoading({
+    title: `上传中...`,
+    mask: true
+  })
+  return uploadFile(cloudPath, filePath)
+    .then(id => {
+      // 获取链接地址
+      return getUrl([id])
+    })
+    .then(res => {
+      const music = res[0]
+      return music.url
+    })
+    .then(res => {
+      wx.showToast({
+        title: '上传成功！',
+        icon: 'none'
+      })
+      return res
+    })
+    .catch(err => {
+      wx.hideLoading()
+      console.log(err)
+    })
+}
+
+const uploadBackground = filePath => {
+  const fileName = getFileName(filePath)
+  const cloudPath = `background/${fileName}`
+  wx.showLoading({
+    title: `上传中...`,
+    mask: true
+  })
+  return uploadFile(cloudPath, filePath)
+    .then(id => {
+      // 获取链接地址
+      return getUrl([id])
+    })
+    .then(res => {
+      return res[0].url
+    })
+    .then(res => {
+      wx.showToast({
+        title: '上传成功！',
+        icon: 'none'
+      })
+      return res
+    })
+    .catch(err => {
+      wx.hideLoading()
+      console.log(err)
+    })
+}
+
+export default {
+  uploadImg,
+  delImg,
+  uploadMusic,
+  uploadBackground
+}
